a300415dc111e743fff0cd5b311210ee3b48614c
shsaxena-mn <30477397+shsaxena-mn@users.noreply.github.com>
2018-09-08 11:24:20 +0530

    adding rooms

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString}=require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -15,9 +16,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.on('join',(param,callback)=>{
+    if(!isRealString(param.name) || !isRealString(param.room)){
+      callback('name and room are required');
+    }
+    socket.join(param.room);
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+    socket.broadcast.to(param.room).emit('newMessage', generateMessage('Admin', `${param.name} has joined`));
+
+      callback();
+  });
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
